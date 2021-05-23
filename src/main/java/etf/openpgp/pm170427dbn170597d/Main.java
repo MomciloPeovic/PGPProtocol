9916465462db67f146fce642f84bf3b98e34bfa7
Momčilo Peović <pm170427d@student.etf.bg.ac.rs>
2021-05-23 19:41:47 +0200

    Added encryption and decryption

--- src/main/java/etf/openpgp/pm170427dbn170597d/Main.java
+++ src/main/java/etf/openpgp/pm170427dbn170597d/Main.java
@@ -1,31 +1,18 @@
 package etf.openpgp.pm170427dbn170597d;
 
-import org.bouncycastle.bcpg.ArmoredOutputStream;
+import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream;
 import org.bouncycastle.bcpg.CompressionAlgorithmTags;
 import org.bouncycastle.bcpg.HashAlgorithmTags;
 import org.bouncycastle.bcpg.SymmetricKeyAlgorithmTags;
-import org.bouncycastle.crypto.params.ElGamalPublicKeyParameters;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.openpgp.*;
-import org.bouncycastle.openpgp.operator.*;
+import org.bouncycastle.openpgp.jcajce.JcaPGPObjectFactory;
 import org.bouncycastle.openpgp.operator.bc.BcPGPContentSignerBuilder;
 import org.bouncycastle.openpgp.operator.bc.BcPGPDataEncryptorBuilder;
-import org.bouncycastle.openpgp.operator.jcajce.JcaPGPContentSignerBuilder;
-import org.bouncycastle.openpgp.operator.jcajce.JcaPGPDigestCalculatorProviderBuilder;
-import org.bouncycastle.openpgp.operator.jcajce.JcaPGPKeyPair;
-import org.bouncycastle.openpgp.operator.jcajce.JcePBESecretKeyEncryptorBuilder;
+import org.bouncycastle.openpgp.operator.jcajce.*;
 
-import javax.crypto.Cipher;
-import javax.crypto.spec.DHParameterSpec;
-import java.io.ByteArrayOutputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.math.BigInteger;
+import java.io.*;
 import java.security.*;
-import java.security.interfaces.DSAPrivateKey;
-import java.security.interfaces.DSAPublicKey;
-import java.security.spec.KeySpec;
 import java.util.Date;
 
 public class Main
@@ -35,76 +22,71 @@ public class Main
 	{
 		Security.addProvider(new BouncyCastleProvider());
 
-		KeyPair dsaKeyPair = DSAKeyGenerator.generateKeyPair(1024);
+		char[] symmetricKey = new char[16];
 
+		String plaintext = args[0];
+		ByteOutputStream byteOutputStream = new ByteOutputStream();
+
+		KeyPair dsaKeyPair = DSAKeyGenerator.generateKeyPair(1024);
+		KeyPair elGamalKeyPair = ElGamalKeyGenerator.generateKeyPair(1024);
+
+		PGPLiteralDataGenerator literalDataGenerator = new PGPLiteralDataGenerator();
+		OutputStream o = literalDataGenerator.open(byteOutputStream, PGPLiteralDataGenerator.TEXT, "text", args[0].length(), new Date());
+		o.write(plaintext.getBytes());
+		o.close();
+
+		byte[] plaintextBytes = byteOutputStream.getBytes();
 
 		PGPSignatureGenerator generator = new PGPSignatureGenerator(new BcPGPContentSignerBuilder(PGPPublicKey.DSA, HashAlgorithmTags.SHA1));
 		generator.init(PGPPublicKey.DSA, new JcaPGPKeyPair(PGPPublicKey.DSA, dsaKeyPair, new Date()).getPrivateKey());
-		generator.update(args[0].getBytes());
+		generator.update(plaintextBytes);
 		PGPSignature signature = generator.generate();
 
-		System.out.println(signature.getEncoded().length);
-
-		ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream(signature.getEncoded().length);
-
+		byteOutputStream = new ByteOutputStream();
 		PGPCompressedDataGenerator compressedDataGenerator = new PGPCompressedDataGenerator(CompressionAlgorithmTags.ZIP);
-		OutputStream o = compressedDataGenerator.open(byteArrayOutputStream, signature.getEncoded());
+		o = compressedDataGenerator.open(byteOutputStream);
+		o.write(signature.getEncoded());
+		o.write(plaintextBytes);
+		o.close();
+
+		byte[] compressedMessageWithSignature = byteOutputStream.getBytes();
+
+		byteOutputStream = new ByteOutputStream();
+		PGPEncryptedDataGenerator encryptedDataGenerator = new PGPEncryptedDataGenerator(new BcPGPDataEncryptorBuilder(SymmetricKeyAlgorithmTags.TRIPLE_DES));
+		encryptedDataGenerator.addMethod(new JcePBEKeyEncryptionMethodGenerator(symmetricKey));
+		o = encryptedDataGenerator.open(byteOutputStream, compressedMessageWithSignature.length);
+		o.write(compressedMessageWithSignature);
+		o.close();
+
+		byte[] encryptedMessage = byteOutputStream.getBytes();
 
 
-		PGPDataEncryptorBuilder pgpDataEncryptorBuilder = new BcPGPDataEncryptorBuilder(SymmetricKeyAlgorithmTags.TRIPLE_DES);
-		PGPDataEncryptor encryptor = pgpDataEncryptorBuilder.build(new byte[]{0, 1, 2, 3, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16});
-		o = encryptor.getOutputStream(o);
+
+		InputStream inputStream = new ByteArrayInputStream(encryptedMessage);
+		inputStream = PGPUtil.getDecoderStream(inputStream);
+
+		PGPObjectFactory objectFactory = new JcaPGPObjectFactory(inputStream);
+		PGPEncryptedDataList encryptedDataList = (PGPEncryptedDataList) objectFactory.nextObject();
+		PGPPBEEncryptedData e = (PGPPBEEncryptedData) encryptedDataList.get(0);
+
+		InputStream clear = e.getDataStream(new JcePBEDataDecryptorFactoryBuilder(new JcaPGPDigestCalculatorProviderBuilder().setProvider("BC").build()).setProvider("BC").build(symmetricKey));
 
 
 
 
-//		KeyPair elgKp = ElGamalKeyGenerator.generateKeyPair(1024);
-//
-//		Cipher cipher = Cipher.getInstance("ELGAMAL");
-//		cipher.init(Cipher.ENCRYPT_MODE, elgKp.getPublic());
-//
-//		byte[] cipherText = cipher.doFinal(message.getBytes());
-//
-//		cipher = Cipher.getInstance("ELGAMAL");
-//		cipher.init(Cipher.DECRYPT_MODE, elgKp.getPrivate());
+		objectFactory = new JcaPGPObjectFactory(clear);
+		PGPCompressedData compressedData = (PGPCompressedData) objectFactory.nextObject();
+
+		byte[] decompressedMessageWithSignature = new byte[1000];
+		compressedData.getDataStream().read(decompressedMessageWithSignature);
+
+		objectFactory = new JcaPGPObjectFactory(decompressedMessageWithSignature);
+		PGPSignatureList signatureList = (PGPSignatureList) objectFactory.nextObject();
+		PGPLiteralData literalData = (PGPLiteralData) objectFactory.nextObject();
+
+		byte[] decodedPlaintext = new byte[100];
+		literalData.getDataStream().read(decodedPlaintext);
+		System.out.println(new String(decodedPlaintext));
 	}
-
-	private static void exportKeyPair(
-			OutputStream secretOut,
-			OutputStream publicOut,
-			KeyPair dsaKp,
-			KeyPair elgKp,
-			String identity,
-			char[] passPhrase,
-			boolean armor)
-			throws IOException, InvalidKeyException, NoSuchProviderException, SignatureException, PGPException
-	{
-		if (armor)
-		{
-			secretOut = new ArmoredOutputStream(secretOut);
-		}
-
-		PGPKeyPair dsaKeyPair = new JcaPGPKeyPair(PGPPublicKey.DSA, dsaKp, new Date());
-		PGPKeyPair elgKeyPair = new JcaPGPKeyPair(PGPPublicKey.ELGAMAL_ENCRYPT, elgKp, new Date());
-		PGPDigestCalculator sha1Calc = new JcaPGPDigestCalculatorProviderBuilder().build().get(HashAlgorithmTags.SHA1);
-		PGPKeyRingGenerator keyRingGen = new PGPKeyRingGenerator(PGPSignature.POSITIVE_CERTIFICATION, dsaKeyPair,
-				identity, sha1Calc, null, null, new JcaPGPContentSignerBuilder(dsaKeyPair.getPublicKey().getAlgorithm(), HashAlgorithmTags.SHA1), new JcePBESecretKeyEncryptorBuilder(PGPEncryptedData.AES_256, sha1Calc).setProvider("BC").build(passPhrase));
-
-		keyRingGen.addSubKey(elgKeyPair);
-
-		keyRingGen.generateSecretKeyRing().encode(secretOut);
-
-		secretOut.close();
-
-		if (armor)
-		{
-			publicOut = new ArmoredOutputStream(publicOut);
-		}
-
-		keyRingGen.generatePublicKeyRing().encode(publicOut);
-
-		publicOut.close();
-	}
-
 
 }
